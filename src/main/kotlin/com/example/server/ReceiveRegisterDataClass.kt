0164895ac4cfe304f21630c3cbd9e891e6cd021e
{Liam} <koreaDevLiam@gmail.com>
2023-09-19 22:09:18 +0900

    checkout

--- src/main/kotlin/com/example/server/ReceiveRegisterDataClass.kt
+++ src/main/kotlin/com/example/server/ReceiveRegisterDataClass.kt
@@ -1,5 +1,9 @@
 package com.example.server
 
+/*
+App -> Server
+Register Data Receive를 위한 Class
+ */
 data class ReceiveRegisterDataClass(
     val name: String,
     val id: String,
---
+++ src/main/kotlin/com/example/server/UserEntity.kt
@@ -0,0 +1,38 @@
+package com.example.server
+
+import javax.persistence.*
+
+/*
+MySQL과 연결하기 위한 코드
+UserEntity.kt: "user_info" 테이블과 매핑되는 엔티티 클래스
+ */
+
+@Entity
+@Table(name = "user_info")
+data class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var userIndex: Long? = null,
+
+    @Column(name = "name", length = 15)
+    val name: String? = null,
+
+    @Column(name = "id", length = 30)
+    var id: String? = null,
+
+    @Column(name = "pwd", length = 30)
+    val pwd: String? = null,
+
+    @Column(name = "spiciness_level")
+    val spicinessLevel: Int? = null,
+
+    @Column(name = "portion_size")
+    val portionSize: Int? = null,
+
+    @Column(name = "taste_sensitivity")
+    val tasteSensitivity: Int? = null
+)
+{
+    // 기본 생성자 추가
+    constructor() : this(null, null, null, null, null, null, null)
+}
